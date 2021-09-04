d410a467a5eb99e1143ac0c0c8a4677b940002fd
Jeff Anderson-Lester <viperhq@hotmail.com>
2021-09-04 18:10:32 -0500

    Add abillity to add task

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,15 @@
 "use strict";
+
+//Add Task button elements.
+var buttonEl = document.querySelector("#save-task");
+var taskToDoEl = document.querySelector("#task-to-do");
+
+//Step 2: Wrap it up in a function:
+var createTaskHandler = function () {
+  var listItemEl = document.createElement("li");
+  listItemEl.className = "task-item";
+  listItemEl.textContent = "This is a new task";
+  taskToDoEl.appendChild(listItemEl);
+};
+//Step 1 Button event handler.
+buttonEl.addEventListener("click", createTaskHandler);
